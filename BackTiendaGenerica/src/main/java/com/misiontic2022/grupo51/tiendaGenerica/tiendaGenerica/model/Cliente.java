895dcc16b0cf85a8e1d30deda6b81c7c5c66781a
JLEONR4 <91027173+JLEONR4@users.noreply.github.com>
2021-11-21 23:31:12 -0500

    Add files via upload

---
+++ BackTiendaGenerica/src/main/java/com/misiontic2022/grupo51/tiendaGenerica/tiendaGenerica/model/Cliente.java
@@ -0,0 +1,98 @@
+package com.misiontic2022.grupo51.tiendagenerica.tiendagenerica.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Clientes")
+public class Cliente {
+	
+	@Id
+	private String id;
+	
+	private Long cedula;
+	private String nombrecompleto;
+	private String direccion;
+	private Long telefono;
+	private String correo;
+	
+	
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Cliente(Long cedula, String nombrecompleto, String direccion, Long telefono, String correo) {
+		super();
+		this.cedula = cedula;
+		this.nombrecompleto = nombrecompleto;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.correo = correo;
+	}
+
+
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public Long getCedula() {
+		return cedula;
+	}
+
+
+	public void setCedula(Long cedula) {
+		this.cedula = cedula;
+	}
+
+
+	public String getNombrecompleto() {
+		return nombrecompleto;
+	}
+
+
+	public void setNombrecompleto(String nombrecompleto) {
+		this.nombrecompleto = nombrecompleto;
+	}
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+	public Long getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(Long telefono) {
+		this.telefono = telefono;
+	}
+
+
+	public String getCorreo() {
+		return correo;
+	}
+
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+
+		
+		
+	
+}
